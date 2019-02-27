cb6376656831a2c26179c73338389641d21d611d
Kamil Iskierka <kamil.iskierka@westwing.pl>
2019-02-27 15:53:22 +0100

    [WMS-1703] Unit tests of PHPArray configuration

--- tests/Adapter/PHPArrayAdapterTests.php
+++ tests/Adapter/PHPArrayAdapterTests.php
@@ -1,6 +1,7 @@
 <?php
 
 use Cache\Factory\Adapter\PHPArray;
+use Cache\Factory\Config\Adapter\PHPArray as Config;
 
 class PHPArrayAdapterTests extends BaseAdapterTests
 {
@@ -14,15 +15,61 @@ class PHPArrayAdapterTests extends BaseAdapterTests
      */
     protected $adapterType = 'PHPArray';
 
+    /* @var Cache\Adapter\PHPArray\ArrayCachePool */
+    protected $arrayCacheItemPool;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $arrayAdapterFactory = new PHPArray();
+        $this->arrayCacheItemPool  = $arrayAdapterFactory->make($this->config);
+    }
+
     /**
      * Tests creation of the filesystem cache item pool
      */
     public function testMake()
     {
-        $filesystemAdapterFactory = new PHPArray();
-        $filesystemCacheItemPool  = $filesystemAdapterFactory->make($this->config);
+        $this->assertInstanceOf('Psr\\Cache\\CacheItemPoolInterface', $this->arrayCacheItemPool);
+        $this->assertInstanceOf('\\Cache\\Adapter\\PHPArray\\ArrayCachePool', $this->arrayCacheItemPool);
+    }
 
-        $this->assertInstanceOf('Psr\\Cache\\CacheItemPoolInterface', $filesystemCacheItemPool);
-        $this->assertInstanceOf('\\Cache\\Adapter\\PHPArray\\ArrayCachePool', $filesystemCacheItemPool);
+    /**
+     *  Test cache limit setting
+     */
+    public function testConfigurationOfLimit()
+    {
+        // We are creating as many items as limit says
+        $limit = $this->config[Config::INDEX_LIMIT];
+        if ($limit === null) {
+            $this->markTestSkipped('Limit is not set in configuration');
+        }
+        for ($i = 1; $i <= $limit; $i++) {
+            $this->arrayCacheItemPool->set('variable' . $i, 'value');
+        }
+
+        // We are checking if first item exist - it should
+        $this->assertTrue($this->arrayCacheItemPool->hasItem('variable1'));
+
+        // We add one more item
+        $this->arrayCacheItemPool->set('variable' . $i, 'value');
+
+        // We are checking if first item exist - it should not
+        $this->assertFalse($this->arrayCacheItemPool->hasItem('variable1'));
+    }
+
+    /**
+     * Test if variables from provided array exists in cache
+     */
+    public function testConfigurationOfInitialArray()
+    {
+        $initialCacheArray = $this->config[Config::INDEX_CACHE_ARRAY];
+        if (empty($initialCacheArray)) {
+            $this->markTestSkipped('Initial cache array is not set in configuration');
+        }
+        foreach ($initialCacheArray as $key => $value) {
+            $this->assertEquals($this->arrayCacheItemPool->getItem($key)->get(), $value);
+        }
     }
 }
