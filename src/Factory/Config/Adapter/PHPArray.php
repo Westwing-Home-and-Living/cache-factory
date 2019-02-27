4a98b5b20bc563534a37407b2f1c2debcb528909
Kamil Iskierka <kamil.iskierka@westwing.pl>
2019-02-27 13:10:04 +0100

    [WMS-1703] Array adapter configuration

--- src/Factory/Config/Adapter/PHPArray.php
+++ src/Factory/Config/Adapter/PHPArray.php
@@ -6,6 +6,11 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
 class PHPArray extends AbstractConfig
 {
+
+    const INDEX_LIMIT           = 'limit';
+
+    const INDEX_CACHE_ARRAY     = 'cache';
+
     /**
      * @inheritdoc
      */
@@ -13,6 +18,17 @@ class PHPArray extends AbstractConfig
     {
         $treeBuilder = new TreeBuilder();
         $node        = $treeBuilder->root($this->getAdapterName());
+
+        $node
+            ->children()
+                ->scalarNode(self::INDEX_LIMIT)
+                    ->defaultValue(null)
+                ->end()
+                ->arrayNode(self::INDEX_CACHE_ARRAY)
+                    ->prototype('scalar')
+                    ->end()
+                ->end()
+            ->end();
 
         return $node;
     }
